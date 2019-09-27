8457e384e4157fa568ea1a5cbe9f06e5d80612ae
majimmy88 <majimmy88@gmail.com>
2019-09-26 19:15:36 -0700

    end of day 2

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -1,9 +1,20 @@
 var Parse = {
 
-  server: `http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`,
+  server: `http://127.0.0.1:3000/`,
 
   create: function(message, successCB, errorCB = null) {
-    // todo: save a message to the server
+
+    $.ajax({
+      //uri: '/class/messages/'
+      url: Parse.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: successCB,
+      error: errorCB || function (error) {
+        console.error('chatterbox: Failed to create message', error);
+      }
+    });
   },
 
   readAll: function(successCB, errorCB = null) {
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,30 @@
 var models = require('../models');
+var fs = require('fs');
+var request = require('request');
+var Promise = require('bluebird');
+Promise.promisifyAll(fs);
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: (req, res) => { //elsewhere we will call app.get on a directory???, and the messages.get cb.
+      fs.readDir(folder)
+        .then((fileList) =>{
+          return readAllFiles(fileList);
+        });
+    },
+    // a function which handles a get request for all messages
+    //we will need to import/require fs = fs, promises = bluebird, ...=promisify fs...
+    //use promisified fs.readDir called on message folder.
+    //  .then((messageFileList) => {
+    //    return readAllMessageFiles(messageFileList)
+    //is readAllMessageFiles a built in program or do we define it elsewhere or is this a reference to the model??
+    //    .then((messageData) => {
+    //      res.end(messageData)
+    //    })
+    //   })
+
+
+    post: (req, res) => {} // a function which handles posting a message to the database
   },
 
   users: {
